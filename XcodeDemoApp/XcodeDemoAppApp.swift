2ee74ba0de0e83bead5901e1c64dea0ae4396c85
codelab <malanezi24@twq.idserve.net>
2024-09-26 11:26:55 +0300

    Initial Commit

---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by Maram Rabeh  on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
